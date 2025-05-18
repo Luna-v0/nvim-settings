24693d5c5f6dbf2e08862725f90f86fcac0c6547
Eduardo Dantas Luna <edudaluna2000@gmail.com>
2025-05-18 13:16:27 -0300

    added smart error checks

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -11,3 +11,21 @@ for _, server in ipairs(servers) do
   lspconfig[server].setup {}
 end
 
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = "●", -- Could be '■', '▎', 'x'
+    spacing = 4,
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
+
+vim.o.updatetime = 250 -- Set update time for CursorHold
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    vim.diagnostic.open_float(nil, { focus = false })
+  end,
+})
+
