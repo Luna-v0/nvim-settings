96cf3114a59c11db27dab753809dc6882f697d85
Eduardo Dantas Luna <edudaluna2000@gmail.com>
2025-05-29 22:48:07 -0300

    formatting misc

--- lua/config/misc.lua
+++ lua/config/misc.lua
@@ -1,35 +1,28 @@
 -- 📂 /lua/config/misc.lua
 -- Updated to support indent-blankline version 3
 
-require("nvim-autopairs").setup {}
-require("gitsigns").setup {}
-require("Comment").setup {}
-require("toggleterm").setup {}
-require("todo-comments").setup {}
-require("which-key").setup {}
-require("persistence").setup {}
-require("nvim-tree").setup {}
-require("lualine").setup { options = { theme = "catppuccin" } }
-require("copilot_cmp").setup {}
+require("nvim-autopairs").setup({})
+require("gitsigns").setup({})
+require("Comment").setup({})
+require("toggleterm").setup({})
+require("todo-comments").setup({})
+require("which-key").setup({})
+require("persistence").setup({})
+require("nvim-tree").setup({})
+require("lualine").setup({ options = { theme = "catppuccin" } })
+require("copilot_cmp").setup({})
 require("notify")
 
 -- Indent-blankline v3 config using new ibl module
-require("ibl").setup {
-  indent = {
-    char = "│",
-  },
-  scope = {
-    enabled = true,
-  },
-  exclude = {
-    filetypes = { "dashboard", "NvimTree", "packer", "lsp-installer" },
-    buftypes = { "terminal" },
-  },
-}
-
-
-
-
-
-
-
+require("ibl").setup({
+	indent = {
+		char = "│",
+	},
+	scope = {
+		enabled = true,
+	},
+	exclude = {
+		filetypes = { "dashboard", "NvimTree", "packer", "lsp-installer" },
+		buftypes = { "terminal" },
+	},
+})
