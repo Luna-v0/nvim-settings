6dff361b1a5628ede5fbb607f15f26a74001ed51
Eduardo Dantas Luna <edudaluna2000@gmail.com>
2025-08-03 23:30:34 -0300

    missplaced the removal

--- lua/config/nvim_tree_config.lua
+++ lua/config/nvim_tree_config.lua
@@ -1,60 +1,63 @@
 require("nvim-tree").setup({
-  disable_netrw = true,
-  hijack_netrw = true,
-  
-  sync_root_with_cwd = true,
-  update_focused_file = {
-    enable = true,
-    update_root = true,
-  },
-  view = {
-    width = 30,
-    relativenumber = false,
-    signcolumn = "yes",
-  },
-  renderer = {
-    group_empty = true,
-    highlight_git = true,
-    icons = {
-      show = {
-        file = true,
-        folder = true,
-        folder_arrow = true,
-        git = true,
-      },
-        git = {
-          unstaged = "",
-          staged = "✓",
-          untracked = "?",
-          deleted = "✗",
-          renamed = "➜",
-          unmerged = "═",
-          ignored = "◌",
-        },
-      },
-    },
-  },
-  filters = {
-    dotfiles = false,
-    custom = { ".git", "node_modules", ".cache" },
-  },
-  git = {
-    enable = true,
-    ignore = false,
-    timeout = 400,
-  },
-  actions = {
-    open_file = {
-      quit_on_open = true,
-      resize_window = true,
-      window_picker = {
-        enable = true,
-        chars = "abcdefghijklmnopqrstuvwxyz",
-        exclude = {
-          filetype = { "notify", "terminal", "packer", "qf", "diff", "fugitive", "neogit" },
-          buftype = { "nofile", "terminal", "nowrite", "quickfix" },
-        },
-      },
-    },
-  },
+	disable_netrw = true,
+	hijack_netrw = true,
+
+	sync_root_with_cwd = true,
+	update_focused_file = {
+		enable = true,
+		update_root = true,
+	},
+	view = {
+		width = 30,
+		relativenumber = false,
+		signcolumn = "yes",
+	},
+	renderer = {
+		group_empty = true,
+		highlight_git = true,
+		icons = {
+			show = {
+				file = true,
+				folder = true,
+				folder_arrow = true,
+				git = true,
+			},
+			glyphs = {
+				default = "",
+				git = {
+					unstaged = "",
+					staged = "✓",
+					untracked = "?",
+					deleted = "✗",
+					renamed = "➜",
+					unmerged = "═",
+					ignored = "◌",
+				},
+			},
+		},
+	},
+	filters = {
+		dotfiles = false,
+		custom = { ".git", "node_modules", ".cache" },
+	},
+	git = {
+		enable = true,
+		ignore = false,
+		timeout = 400,
+	},
+	actions = {
+		open_file = {
+			quit_on_open = true,
+			resize_window = true,
+			window_picker = {
+				enable = true,
+				chars = "abcdefghijklmnopqrstuvwxyz",
+				exclude = {
+					filetype = { "notify", "terminal", "packer", "qf", "diff", "fugitive", "neogit" },
+					buftype = { "nofile", "terminal", "nowrite", "quickfix" },
+				},
+			},
+		},
+	},
 })
+
